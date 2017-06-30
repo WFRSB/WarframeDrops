7b8f88a64233b1aa0a294bf5fd56ad2f459abfa1
pegasy <pegasy@users.noreply.github.com>
2017-06-30 22:55:28 +0000

    Update 2017-06-30 22:55:28

--- update.sh
+++ update.sh
@@ -3,7 +3,7 @@ git pull
 date=$(date +"%Y-%m-%d %H:%M:%S")
 url="https://n8k6e2y6.ssl.hwcdn.net/repos/hnfvc0o3jnfvc873njb03enrf56.html"
 wget -qO warframe-drops.html "$url"
-tidy -config tidy_config.txt -o warframe-drops.html warframe-drops-pretty.html
+tidy -config tidy_config.txt -o warframe-drops-pretty.html warframe-drops.html
 git commit -am "Update ${date}"
 git push
 
