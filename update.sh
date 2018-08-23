c788b2ec3656ec44ca3f7718c511948e145777e8
pegasy <pegasy@users.noreply.github.com>
2018-08-23 21:25:00 +0200

    Update 2018-08-23 21:24:58

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 git pull
 date=$(date +"%Y-%m-%d %H:%M:%S")
-url="https://n8k6e2y6.ssl.hwcdn.net/repos/hnfvc0o3jnfvc873njb03enrf56.html"
-wget -qO warframe-drops.html "$url"
-tidy -config tidy_config.txt -o warframe-drops-pretty.html warframe-drops.html
+docker build -t pegasy/warframedrops .
+
+docker run --rm -v $(pwd):/data pegasy/warframedrops
 git commit -am "Update ${date}"
 git push
 
