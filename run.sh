c90fb2689a1d7aad78be61a426460ab1cf18ebcf
pegasy <pegasy@users.noreply.github.com>
2019-03-16 13:45:10 +0100

    Update 2019-03-16 13:45:04

--- run.sh
+++ run.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-url="https://n8k6e2y6.ssl.hwcdn.net/repos/hnfvc0o3jnfvc873njb03enrf56.html"
-wget -qO warframe-drops.html "$url"
+wget -qO warframe-drops.html "https://n8k6e2y6.ssl.hwcdn.net/repos/hnfvc0o3jnfvc873njb03enrf56.html"
 tidy -config tidy_config.txt -o warframe-drops-pretty.html warframe-drops.html
 
