a5ef1e7cdcd5a6ff6b787218b38a04d2f560fe8e
pegasy <pegasy@users.noreply.github.com>
2019-03-30 22:04:39 +0100

    move files around and add riven trading json

--- update.sh
+++ update.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-git pull
 date=$(date +"%Y-%m-%d %H:%M:%S")
 docker build -t pegasy/warframedrops .
 
 docker run --rm -v $(pwd):/data pegasy/warframedrops
 git commit -am "Update ${date}"
+git pull
 git push
 
